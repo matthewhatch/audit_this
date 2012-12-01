4955c32520dec7548eed564496bb2e250a68b5f8
Matt Hatch <mhatch73@gmail.com>
2012-11-30 21:42:54 -0500

    moved code to audit_this/audit_this.rb

--- lib/audit_this.rb
+++ lib/audit_this.rb
@@ -1,17 +1,6 @@
 require "active_record"
 require "audit_this/audit"
 require "audit_this/version"
+require "audit_this/audit_this"
 
-module AuditThis
-  def self.add_item(audit)
-    #create new audit record
-  end
 
-  def self.add_items
-    #create several audit records
-  end
-
-  def self.remove_item
-
-  end
-end
---
+++ spec/audit_this/audit_this.rb
@@ -0,0 +1,13 @@
+module AuditThis
+  def self.add_item(audit)
+    #create new audit record
+  end
+
+  def self.add_items
+    #create several audit records
+  end
+
+  def self.remove_item
+
+  end
+end
